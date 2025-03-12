6e665795713d442698eb2937cfa2bfcd48cf370c
seoky99 <seokyoung99@gmail.com>
2025-03-12 16:28:16 +0900

    final polish to styling (form placement, cards), added docu

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ function addBookToLibrary(author, title, numPages, beenRead) {
     myLibrary.push(book); 
 }
 
+/*
+    Creates a card for a child that has properties depending on 
+    its Book values. 
+*/
 function createChild(elt) {
     let item = document.createElement("div");
     item.classList.add("card"); 
@@ -30,15 +34,15 @@ function createChild(elt) {
     children.push(header); 
 
     let para = document.createElement("p");
-    para.innerHTML = elt.author; 
+    para.innerHTML = `Written by ${elt.author}`; 
     children.push(para); 
 
     let para2 = document.createElement("p");
-    para2.innerHTML = elt.numPages; 
+    para2.innerHTML = `${elt.numPages} pages`; 
     children.push(para2); 
 
     let beenRead = document.createElement("p"); 
-    beenRead.innerHTML = elt.beenRead; 
+    beenRead.innerHTML = elt.beenRead ? "Read" : "Not read"; 
     beenRead.id = "beenRead";
     children.push(beenRead); 
 
@@ -52,23 +56,36 @@ function createChild(elt) {
     toggleButton.id = "toggle";
     children.push(toggleButton);
 
-    children.forEach( (child) => item.appendChild(child));
+    children.forEach((child) => item.appendChild(child));
     return item; 
 }
 
+/*
+    Replaces the entire container and displays everything currently in Library.
+*/
 function addToDisplay() {
-    let cardContainer = document.querySelector(".card-container");
+    let oldCardContainer = document.querySelector(".card-container");
+    let newCardContainer = document.createElement("div");
+    newCardContainer.className = "card-container";
+    oldCardContainer.parentNode.replaceChild(newCardContainer, oldCardContainer);
+
     myLibrary.forEach( (elt) => {
         let item = createChild(elt);
-        cardContainer.appendChild(item);
+        newCardContainer.appendChild(item);
     })
 }
 
+/*
+    Simply appends a child to the display 
+*/
 function addOne(elt) {
     let cardContainer = document.querySelector(".card-container");
     cardContainer.appendChild(createChild(elt));
 }
 
+/*
+    Adds a book into the Library after the form is submitted and modifies display 
+*/
 function formSubmit() {
 
     let form = document.querySelector("form"); 
@@ -85,7 +102,10 @@ function formSubmit() {
     })
 }
 
-function deleteChild() {
+/*
+    Handles buttons in child cards to delete or toggle beenRead
+*/
+function clickChild() {
     let parent = document.querySelector(".card-container");
 
     parent.addEventListener("click", (e) => {
@@ -98,24 +118,15 @@ function deleteChild() {
             myLibrary.forEach(elt => {
                 if (elt.id === id) {
                     elt.toggleMe(); 
-                    console.log(myLibrary);
-
-                    let x = document.querySelector(`.card[data-id="${id}"] p#beenRead`); 
-                    x.innerHTML = elt.beenRead; 
+                    let beenReadP = document.querySelector(`.card[data-id="${id}"] p#beenRead`); 
+                    beenReadP.innerHTML = elt.beenRead ? "Read" : "Not read";  
                 }
             })
-      
         }
     }) 
 }
 
-function toggleChild() {
-
-
-
-}
-
+addToDisplay();
 formSubmit();
-deleteChild(); 
-toggleChild(); 
+clickChild();
 
